9fe2986bd0be3de7c1039faabde50a11d3fffa26
ravi kumar <Ravi7481081raj@gmail.com>
2026-07-25 18:52:00 +0530

    Time: 2267 ms (17.29%), Space: 47.3 MB (17.29%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -3,28 +3,20 @@ import java.util.*;
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         Arrays.sort(nums);
-
-        ArrayList<Integer> ar = new ArrayList<>();
-        for (int num : nums) {
-            if (ar.isEmpty() || ar.get(ar.size() - 1) != num) {
-                ar.add(num);
-            }
-        }
-
         HashSet<Integer> pair = new HashSet<>();
 
-        int m = ar.size();
-
+        
+        int m=nums.length;
         for (int i = 0; i < m; i++) {
             for (int j = i; j < m; j++) {
-                pair.add(ar.get(i) ^ ar.get(j));
+                pair.add(nums[i] ^ nums[j]);
             }
         }
 
         HashSet<Integer> ans = new HashSet<>();
 
         for (int x : pair) {
-            for (int v : ar) {
+            for (int v : nums) {
                 ans.add(x ^ v);
             }
         }
