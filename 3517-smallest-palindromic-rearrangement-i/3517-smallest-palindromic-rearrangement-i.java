e4cf6fb1747f65bccb249b58bb8214485f2f7e87
ravi kumar <Ravi7481081raj@gmail.com>
2026-07-28 19:16:07 +0530

    Time: 35 ms (31.33%), Space: 48 MB (75.9%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,44 +1,37 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        int[] ar=new int[26];
+        int[] ar = new int[26];
+
+        for (char c : s.toCharArray()) {
+            ar[c - 'a']++;
+        }
+
+        StringBuilder left = new StringBuilder();
+        StringBuilder right = new StringBuilder();
+        char middle = 0;
+
+        for (int i = 0; i < 26; i++) {
+            int count = ar[i];
+
+            if (count % 2 == 1) {
+                middle = (char) ('a' + i);
+            }
+
+            int k = count / 2;
+            while (k > 0) {
+                left.append((char) ('a' + i));
+                k--;
+            }
+        }
+
+       
+        right.append(left).reverse();
+
         
-        for(char c:s.toCharArray()){
-            int o=c-'a';
-            ar[o]++;
+        if (middle != 0) {
+            return left.toString() + middle + right.toString();
+        } else {
+            return left.toString() + right.toString();
         }
-        StringBuilder sr=new StringBuilder();
-        int odd=-1;
-    
-        for(int i=0;i<26;i++){
-            if(ar[i]!=0 && ar[i]>=2){
-                int k=ar[i]/2;
-                while(k>0){
-                    char c=(char)('a'+i);
-                    sr.append(c);
-                    k--;
-                }
-            }
-            if(ar[i]%2==1){
-                odd=i;
-            }
-            
-        }
-        if(odd!=-1){
-             char c=(char)('a'+odd);
-            sr.append(c);
-        }
-         for(int i=25;i>=0;i--){
-            if(ar[i]!=0 ){
-                int k=ar[i]/2;
-                while(k>0){
-                    char c=(char)('a'+i);
-                    sr.append(c);
-                    k--;
-                }
-            }
-         }
-         return sr.toString();
-
-
     }
 }
