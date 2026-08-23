790e61f0a5dd792a151a4c28c11de2c461572a5f
ravi kumar <Ravi7481081raj@gmail.com>
2026-08-23 18:27:59 +0530

    Time: 0 ms (100%), Space: 43.1 MB (99.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,41 +1,42 @@
 class Solution {
-    public int search(int[] nums) {
-        int mid;
-        int start=0, end=nums.length-1;
-        while(start<end){
-            mid=(start+end)/2;
-            if(nums[mid]>nums[end]){
-                start=mid+1;
-            }else{
-                end=mid;
-            }
-        }
-        return end ;
-    }
-    public int search1(int[] nums, int target, int start,int end) {
-        int mid;
-        
-        while(start<=end){
-            mid=(start+end)/2;
-            if(nums[mid]==target){
+
+    public int binary(int[] nums, int i, int j, int target) {
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+
+            if (nums[mid] == target) {
                 return mid;
-            }else if(nums[mid]<target){
-                start=mid+1;
-            }else{
-                end=mid-1;
+            } else if (nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
             }
         }
         return -1;
     }
-    public int search(int[] nums, int target) {
-        int pivot=search(nums);
-       
 
-        if ( target <= nums[nums.length - 1]) {
-            return search1(nums, target, pivot, nums.length - 1);
-        } else {
-            return search1(nums, target, 0, pivot - 1);
+    public int piv(int[] nums, int i, int j) {
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+
+            if (nums[mid] > nums[j]) {
+                i = mid + 1;
+            } else {
+                j = mid;
+            }
         }
-        
+        return i;
+    }
+
+    public int search(int[] nums, int target) {
+        int n = nums.length - 1;
+
+        int pivot = piv(nums, 0, n);
+
+        if (target >= nums[pivot] && target <= nums[n]) {
+            return binary(nums, pivot, n, target);
+        } else {
+            return binary(nums, 0, pivot - 1, target);
+        }
     }
 }
