86bab975636896df78f63cd0c784577ab192fb77
ravi kumar <Ravi7481081raj@gmail.com>
2026-08-28 20:40:00 +0530

    Time: 84 ms (7.03%), Space: 144 MB (67.22%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,52 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> nums[b] - nums[a]
+        );
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        int[] ar = new int[nums.length - k + 1];
+        int j = 0;
 
-        int n = nums.length;
-        int[] ans = new int[n - k + 1];
-
-        int p = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            maxHeap.offer(nums[i]);
-
-            map.computeIfAbsent(nums[i], x -> new ArrayList<>()).add(i);
-
-            int left = i - k + 1;
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(i);
 
             if (i >= k - 1) {
+                while (pq.peek() <= i - k) {
+                    pq.poll();
+                }
 
-               while (!maxHeap.isEmpty()) {
-
-    int val = maxHeap.peek();
-    ArrayList<Integer> list = map.get(val);
-
-    if (list == null) {
-        maxHeap.poll();
-        continue;
-    }
-
-    while (!list.isEmpty() && list.get(0) < left) {
-        list.remove(0);
-    }
-
-    if (list.isEmpty()) {
-        map.remove(val);
-        maxHeap.poll();
-    } else {
-        break;
-    }
-}
-
-                ans[p++] = maxHeap.peek();
+                ar[j++] = nums[pq.peek()];
             }
         }
 
-        return ans;
+        return ar;
     }
 }
