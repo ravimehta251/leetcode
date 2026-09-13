8424966fe7d5c383108f9df39c101a4917674ce3
ravi kumar <Ravi7481081raj@gmail.com>
2026-09-13 12:10:19 +0530

    Time: 67 ms (11.55%), Space: 43 MB (18.49%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,23 +1,46 @@
 class Solution {
+
+    int solve(int i, int j, int[] nums) {
+
+        // No elements left
+        if (i > j) {
+            return 0;
+        }
+
+        // Only one element
+        if (i == j) {
+            return nums[i];
+        }
+
+        // Pick left
+        int take_i = nums[i] + Math.min(
+            solve(i + 2, j, nums),
+            solve(i + 1, j - 1, nums)
+        );
+
+        // Pick right
+        int take_j = nums[j] + Math.min(
+            solve(i, j - 2, nums),
+            solve(i + 1, j - 1, nums)
+        );
+
+        return Math.max(take_i, take_j);
+    }
+
     public boolean predictTheWinner(int[] nums) {
+
         int n = nums.length;
-        int[][] dp = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = nums[i];
+        int total = 0;
+
+        for (int x : nums) {
+            total += x;
         }
 
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
+        int player1 = solve(0, n - 1, nums);
 
-                dp[i][j] = Math.max(
-                    nums[i] - dp[i + 1][j],
-                    nums[j] - dp[i][j - 1]
-                );
-            }
-        }
+        int player2 = total - player1;
 
-        return dp[0][n - 1] >= 0;
+        return player1 >= player2;
     }
 }
